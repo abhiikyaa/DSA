62b3b556c612d4147533aa52e6aded3f2f393901
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2025-12-09 10:59:58 +0530

    Time: 2 ms (96.03%), Space: 72.4 MB (20.28%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0,twenty = 0;
+        for (int i =0; i<bills.length; i++){
+            if(bills[i]==5){
+                five++;
+            }
+            else if(bills[i]==10) {
+                ten++;
+                if(five<1){
+                    return false;
+                }
+                else{
+                    five--;
+                }
+            }
+            else{
+                if(five>0 && ten>0){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else{
+                    return false;
+                }
+
+
+            }
+
+        }
+        return true;
+    }
+}
