d9974bb31f9b6258c8df99fe52750e5f7ab0f678
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-02-11 10:50:44 +0530

    Time: 6 ms (23.16%), Space: 51.6 MB (81.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,45 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0]. length == 0 ){
-            return 0;
+    class Pair{
+        int row;
+        int col;
+        Pair(int row, int col){
+            this.row = row;
+            this.col = col;
         }
-        int count = 0;
-
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid , i , j);
+    }
+    int direction[][] = {{-1,0},{1,0},{0,-1},{0,1}};
+    int count = 0;
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1' && visited[i][j] == false){
                     count++;
+                    bfs(visited, grid, i, j , n , m);
                 }
             }
         }
         return count;
     }
-    public void dfs(char[][] grid, int i, int j){
-        if(i < 0 || i >= grid.length || j< 0 || j >= grid[0].length || grid[i][j] == '0'){
-            return;
-        }
-        grid[i][j] = '0';
-        dfs(grid , i+1 , j);
-        dfs(grid , i-1 , j);
-        dfs(grid , i , j+1);
-        dfs(grid , i , j-1);
+    public void bfs(boolean[][] visited,char[][] grid, int currRow, int currCol, int tRow, int tCol){
+        Queue <Pair> q = new LinkedList<>();
+        q.add(new Pair(currRow, currCol));
+        visited[currRow][currCol] = true;
 
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+
+            for(int[] dir : direction){
+                int nRow = curr.row + dir[0];
+                int nCol = curr.col + dir[1];
+
+                if (nRow >= 0 && nRow < tRow && nCol >= 0 && nCol < tCol &&grid[nRow][nCol] == '1' && !visited[nRow][nCol]) {
+                    visited[nRow][nCol] = true;
+                    q.add(new Pair(nRow, nCol));
+                }
+            }    
+        }      
     }
 }
