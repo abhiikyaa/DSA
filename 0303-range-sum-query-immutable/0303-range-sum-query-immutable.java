6b36be64950275f4045e7fef446f9331483db209
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-03-17 11:19:14 +0530

    Time: 10 ms (20.07%), Space: 51.1 MB (5.78%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,60 +1,65 @@
 class NumArray {
     class SegmentTree{
-        int segTree[];
+        int[] seg;
         SegmentTree(int n){
-            segTree=new int[4*n];
+            seg = new int[4*n];
         }
-
-        void buildTree(int []arr,int idx,int start, int end){
-
-            if(start==end){
-                segTree[idx]=arr[start];
+        void buildTree(int[] arr, int idx, int start, int end){
+            if(start == end){
+                seg[idx] = arr[start];
                 return;
             }
-
-            int mid=(start+end)/2;
+            int mid = (start+end)/2;
             buildTree(arr,2*idx+1,start,mid);
             buildTree(arr,2*idx+2,mid+1,end);
 
-            segTree[idx]=segTree[2*idx+1]+segTree[2*idx+2];
+            seg[idx] = seg[2*idx+1] + seg[2*idx+2];
         }
-        void update(int idx,int i,int val,int start, int end){
-
-            if(start==end){
-                segTree[idx]=val;
+        void update(int idx, int i, int val, int start , int end){
+            if(start == end){
+                seg[idx] = val;
                 return;
             }
-            int mid=(start+end)/2;
-
-            if(i<=mid) update(2*idx+1,i,val,start,mid);
+            int mid = (start+end)/2;
+            if(i <= mid){
+                update(2*idx+1,i,val,start,mid);
+            }
             else{
                 update(2*idx+2,i,val,mid+1,end);
             }
-            segTree[idx]=segTree[2*idx+1]+segTree[2*idx+2];
+
+            seg[idx] = seg[2*idx+1] + seg[2*idx+2];
         }
-        int query(int idx,int qStart,int qEnd,int start, int end){
-            if(start>=qStart && end<=qEnd) return segTree[idx];
 
-            if(end<qStart || start>qEnd) return 0;
+        int query(int idx, int qStart, int qEnd, int start, int end){
+            if(start >= qStart && end <= qEnd){
+                return seg[idx];
+            }
+            if(end < qStart || start > qEnd){
+                return 0;
+            }
+            int mid = (start+end)/2;
 
-            int mid=(start+end)/2;
-
-            int left=query(2*idx+1,qStart,qEnd,start,mid);
-            int right=query(2*idx+2,qStart,qEnd,mid+1,end);
-
-            return left+right;
+            int leftside = query(2*idx+1,qStart,qEnd,start,mid);
+            int rightside = query(2*idx+2,qStart,qEnd,mid+1,end);
+            return leftside +rightside;
         }
     }
-
     SegmentTree st;
     int n;
+
     public NumArray(int[] nums) {
-        n=nums.length;
-        st=new SegmentTree(n);
+        n = nums.length;
+        st = new SegmentTree(n);
         st.buildTree(nums,0,0,n-1);
     }
-    
-    public int sumRange(int left, int right) {
+    public int sumRange(int left, int right){
         return st.query(0,left,right,0,n-1);
     }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
