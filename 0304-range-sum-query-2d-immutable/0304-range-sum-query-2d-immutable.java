d2884f93636c14a5b75b7f6bdd85482794e9db27
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-03-20 11:15:33 +0530

    Time: 111 ms (30.72%), Space: 141.9 MB (69.78%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,21 +1,43 @@
 class NumMatrix {
-    int[][] p;
-
+    int prefix[][];
     public NumMatrix(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        p = new int[n+1][m+1];
+        prefix = new int[n][m];
+        prefix[0][0] = matrix[0][0];
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                p[i][j] = matrix[i-1][j-1] - p[i-1][j-1] + p[i-1][j] + p[i][j-1];
+        for(int i=1;i<n;i++){
+            prefix[i][0] = prefix[i-1][0]+matrix[i][0];
+
+        }
+
+        for(int i=1;i<m;i++){
+            prefix[0][i] = prefix[0][i-1]+matrix[0][i];
+
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]+matrix[i][j];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return p[row2 + 1][col2 + 1] - p[row1][col2 + 1] - p[row2 + 1][col1] + p[row1][col1];
+        if(row1==0 && col1==0) return prefix[row2][col2];
+
+        else if(row1==0 && col1>0){
+            return prefix[row2][col2]-prefix[row2][col1-1];
+        }
+
+        else if(row1>0 && col1==0){
+            return prefix[row2][col2]-prefix[row1-1][col2];
+        }
+        else{
+            return prefix[row2][col2]-prefix[row1-1][col2]-prefix[row2][col1-1]+prefix[row1-1][col1-1];
+        }
+        
     }
 }
 
