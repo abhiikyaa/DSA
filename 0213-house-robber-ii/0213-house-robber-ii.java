f35f51723428ce79ac0d24a2a746008569d67eeb
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-03-29 15:49:08 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.82%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,36 +1,44 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
+        if(nums.length == 0){
+            return 0;
+        }
+        if(nums.length == 1){
+            return nums[0];
+        }
+        int[] dp1 = new int[nums.length + 1];
+        Arrays.fill(dp1 , -1);
 
-        int[] dp1 = new int [n];
-        Arrays.fill(dp1,-1);
-        int case1 =  MaxAmt(nums, n-2, dp1);
+        int[] dp2 = new int[nums.length + 1];
+        Arrays.fill(dp2 , -1);
 
-        int[] dp2 = new int[n];
-        Arrays.fill(dp2,-1);
-        int case2 =  MaxAmt2(nums,n-1,dp2);
-
-        return Math.max(case1,case2);
+        return Math.max(MaxAmt1(nums , nums.length-2, dp1) , MaxAmt2(nums , nums.length-1 , dp2));
     }
-    public int MaxAmt(int[] nums, int n, int[] dp){
-        if(n<0) return 0;
+    public int MaxAmt1(int[] nums, int n, int[] dp1){
+        if(n < 0) return 0;
+
         if(n == 0) return nums[0];
+
         if(n == 1) return Math.max(nums[0] , nums[1]);
-        if(dp[n] != -1) return dp[n];
 
-        dp[n] = Math.max(nums[n] + MaxAmt(nums, n-2, dp) , MaxAmt(nums, n - 1 , dp));
-        return dp[n];
+        if(dp1[n] != -1) return dp1[n];
+
+        dp1[n] = Math.max(nums[n] + MaxAmt1(nums, n-2, dp1) , MaxAmt1(nums , n-1 , dp1));
+
+        return dp1[n];
     }
-    public int MaxAmt2(int[] nums, int n , int[] dp){
+
+    public int MaxAmt2(int[] nums , int n , int[] dp2){
         if(n < 1) return 0;
+
         if(n == 1) return nums[1];
+
         if(n == 2) return Math.max(nums[1] , nums[2]);
-        if(dp[n] != -1) return dp[n];
 
-        dp[n] = Math.max(nums[n] + MaxAmt2(nums, n-2, dp) , MaxAmt2(nums, n - 1 , dp));
-        return dp[n];
+        if(dp2[n] != -1) return dp2[n];
 
+        dp2[n] = Math.max(nums[n] + MaxAmt2(nums , n-2 , dp2) , MaxAmt2(nums , n -1, dp2));
+
+        return dp2[n];
     }
 }
