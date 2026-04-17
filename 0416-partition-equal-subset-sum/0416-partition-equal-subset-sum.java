ca2975f18bfb939c7ecc5679c3f1d97e2566088d
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-04-17 15:02:48 +0530

    Time: 21 ms (92.45%), Space: 75.4 MB (20.81%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,26 +1,20 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum =0;
-        for(int num:nums){
+        int sum = 0;
+        for(int num : nums){
             sum += num;
         }
-        if(sum%2 != 0){
-            return false;
-        }  
-        Boolean dp[][]= new Boolean[nums.length][(sum/2) +1];
-        return canPartitionRecursive(nums,0,sum/2, dp);
+        if(sum%2 != 0) return false;;
+        Boolean[][] dp = new Boolean[nums.length+1][(sum/2)+1];
+        return helper(nums,0,sum/2,dp);
     }
-    public boolean canPartitionRecursive(int[] nums,int i, int target,Boolean dp[][]){
-        if (target == 0){
-            return true;
-        }
-        if(i >= nums.length || target < 0){
-            return false;
-        }
-        if(dp[i][target] != null){
-            return dp[i][target];
-        }
-        dp[i][target] = canPartitionRecursive(nums,i+1,target-nums[i],dp) || canPartitionRecursive(nums,i+1,target,dp);
-        return dp[i][target];
+    public boolean helper(int[] nums, int i, int t, Boolean[][] dp){
+        if(t == 0) return true;
+        if(i >= nums.length || t < 0) return false;
+        if(dp[i][t] != null) return dp[i][t];
+
+        dp[i][t] = (helper(nums,i+1,t - nums[i], dp) || helper(nums,i+1,t , dp));
+        return dp[i][t];
+
     }
 }
