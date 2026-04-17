c8360140a5a6473e45ba0d69d3efea380aaae509
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-04-17 14:14:28 +0530

    Time: 40 ms (95.26%), Space: 207.6 MB (55.92%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
@@ -1,19 +1,16 @@
 class Solution {
     public int maxTwoEvents(int[][] events) {
-        //sort based on end time
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1]-b[1]);
-        //sort based on start time
         Arrays.sort(events,(a,b)-> a[0]-b[0]);
         int preMax = 0;
         int ans = 0;
-        for(int[] event : events){
-            //find preMax
-            while(!pq.isEmpty() && pq.peek()[1] < event[0]){
+        for(int[] e : events){
+            while(!pq.isEmpty() && pq.peek()[1] < e[0]){
                 preMax = Math.max(preMax, pq.peek()[2]);
                 pq.poll();
             }
-            ans = Math.max(ans, preMax + event[2]);
-            pq.offer(event);
+            ans = Math.max(ans,preMax + e[2]);
+            pq.offer(e);
         }
         return ans;
     }
