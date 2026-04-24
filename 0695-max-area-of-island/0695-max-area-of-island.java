8cf2e2ea13ba90b1bc5942ef7784caafde2b6262
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-04-24 08:22:57 +0530

    Time: 1 ms (100%), Space: 46.5 MB (51.88%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,49 +1,27 @@
 class Solution {
-    class Pair{
-        int row;
-        int col;
-        Pair(int row, int col){
-            this.row = row;
-            this.col = col;
-        }
-    }
-    int  dir[][] = {{-1,0},{1,0},{0,-1},{0,1}};
-    int max = 0;
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1){
-                    int count = bfs(grid, i, j);
-                    max = Math.max(max, count);
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    sum = Math.max(sum,dfs(grid,i,j));
                 }
             }
         }
-        return max;  
+        return sum;
     }
-    public int bfs(int[][] grid, int r, int c){
-        Queue <Pair> q = new LinkedList<>();
-        q.add(new Pair(r,c));
-        grid[r][c] = 0;
-        int count = 1;
-
-        while(!q.isEmpty()){
-            Pair curr = q.poll();
-
-            for(int[] d : dir){
-                int nr = curr.row + d[0];
-                int nc = curr.col + d[1];
-
-                if(nr >= 0 && nc >=0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 1){
-                    grid[nr][nc] = 0;
-                    q.add(new Pair(nr,nc));
-                    count++;
-                }
-            }
+    static int dfs(int[][] grid,int i,int j){
+        int n = grid.length;
+        int m = grid[0].length;
+        if(i<0||j<0||i>=n||j>=m||grid[i][j]==0){
+            return 0;
         }
-        return count;
+        grid[i][j]=0;
+        return 1+dfs(grid,i+1,j)+
+                dfs(grid,i-1,j)+
+                dfs(grid,i,j+1)+
+                dfs(grid,i,j-1);
     }
-    
 }
