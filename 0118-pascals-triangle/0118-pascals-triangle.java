6b26018fc7357715881c4ef607b7f158df56264f
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-04-30 21:38:14 +0530

    Time: 1 ms (95.52%), Space: 43.4 MB (70.86%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<Integer> firstrow = new ArrayList<>();
+        firstrow.add(1);
+        result.add(firstrow);
+        if(numRows == 1){
+            return result;
+        }
+
+        for(int i = 1 ;i<numRows;i++){
+            List<Integer>pevrow = result.get(i-1);
+
+            ArrayList<Integer>row = new ArrayList<>();
+            row.add(1);
+
+            for(int j = 0;j<i-1;j++){
+                row.add(pevrow.get(j)+pevrow.get(j+1));    
+            }
+            row.add(1);
+
+            result.add(row);
+        }
+        return result;
+
+    }
+}
