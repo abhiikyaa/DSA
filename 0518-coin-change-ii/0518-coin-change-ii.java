c6ef264a8b9b900ee1ca869c635a21a63d02ff46
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-05-11 21:40:26 +0530

    Time: 33 ms (8.85%), Space: 56.5 MB (47.47%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,25 +1,33 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        Integer[][] dp = new Integer[n] [amount + 1];
-        return mcc(n-1, amount, coins, dp);
+        int[][] dp = new int[n+1][amount+1];
+        for(int i = 0; i < n; i++)
+        {
+            Arrays.fill(dp[i],-1);
+        }
+        return sol(0,amount,coins,dp);
     }
 
-    public int mcc(int idx, int target, int[] coins, Integer[][] dp){
-        if(target == 0){
+    public int sol(int i, int j, int[] coins,int[][] dp)
+    {
+        if(j == 0)
+        {
             return 1;
         }
-        if(idx  < 0){
+        if(i < 0)
+        {
             return 0;
         }
-        if(dp[idx][target] != null){
-            return dp[idx] [target]; 
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int take = 0;
+        int skip = sol(i+1,j,coins,dp);
+        if(coins[i] <= j)
+        {
+            take = sol(i,j-coins[i],coins,dp);
         }
-        int notpick = mcc (idx - 1, target, coins, dp);
-        int pick = 0;
-        if(coins[idx] <= target){
-            pick = mcc(idx, target - coins[idx], coins, dp);
-        }
-        return dp[idx] [target] = pick + notpick;
+        return dp[i][j] = take + skip;
     }
 }
