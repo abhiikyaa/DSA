a8f31031f825c9d6bcd36901ac67ce768499f02c
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-05-11 17:16:22 +0530

    Time: 16 ms (64.12%), Space: 46.9 MB (32.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,29 +1,66 @@
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         int n = coins.length;
+//         int[][] dp = new int[n+1][amount+1];
+//         for(int i = 0; i <n; i++)
+//         {
+//             Arrays.fill(dp[i],-1);
+//         }
+//         for(int i = 1; i <= n; i++)
+//         {
+//             for(int j = 0; j <= amount; j++)
+//             {
+//                 dp[i][0] = 0;
+//                 dp[0][j] = (int) 1e9;
+//                 int take = (int) 1e9;
+//                 int nottake = dp[i+1][j];
+//                 if(coins[i] <= j)
+//                 {
+//                     take = dp[i][j-coins[i]] + 1;
+//                 }
+//                 return dp[i][j] = Math.min(take,nottake);
+//             }
+//         }
+//         int ans = dp[0][amount];
+//         if(ans >= (int)1e9){
+//             return -1;
+//         }
+//         else{
+//             return ans;
+//         }
+//     }
+// }
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
-        for(int i = 0; i < n; i++)
-        {
-            Arrays.fill(dp[i],-1);
-        }
-        int ans = solve(coins,n-1,amount,dp);
-        if(ans >= (int) 1e9){
-            return -1;
-        }else return ans;
-    }
-    public int solve(int[] coins, int i, int j, int[][] dp)
-    {
-        if(j == 0) return 0;
-        if(i < 0) return (int) 1e9;
-        
-        if(dp[i][j] != -1) return dp[i][j];
+        int[][] dp = new int[n+1][amount+1];
 
-        int take = (int) 1e9;
-        int nottake = solve(coins,i-1,j,dp);
-        if(coins[i] <= j)
-        {
-            take = 1 + solve(coins,i,j-coins[i],dp);
+        for(int j = 1; j <= amount; j++) {
+            dp[n][j] = (int)1e9; 
         }
-        return dp[i][j] = Math.min(take , nottake);
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = 0; j <= amount; j++) {
+
+                int notTake = dp[i+1][j];
+                int take = (int)1e9;
+
+                if(coins[i] <= j) {
+                    take = 1 + dp[i][j - coins[i]];
+                }
+
+                dp[i][j] = Math.min(take, notTake);
+            }
+        }
+
+        int ans = dp[0][amount];
+        if(ans == (int)1e9)
+        {
+            return -1;
+        }
+        else
+        {
+            return ans;
+        }
     }
 }
