eb33237a39c17f4a3ae49ab474474cd43e204b59
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-05-11 22:00:51 +0530

    Time: 14 ms (37.91%), Space: 56.8 MB (38.62%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,33 +1,25 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        for(int i = 0; i < n; i++)
+        int[][] dp =  new int[n+1][amount+1];
+        for(int i = 0; i <= n; i++)
         {
-            Arrays.fill(dp[i],-1);
+            dp[i][0] = 1;
         }
-        return sol(0,amount,coins,dp);
-    }
+        for(int i = 1; i <= n; i++)
+        {    
+            for(int j = 1; j <= amount; j++)
+            {
+                int take  = 0;
+                int skip = dp[i-1][j];
 
-    public int sol(int i, int j, int[] coins,int[][] dp)
-    {
-        if(j == 0)
-        {
-            return 1;
+                if(coins[i-1] <= j)
+                {
+                    take = dp[i][j-coins[i-1]];
+                }
+                dp[i][j] = take + skip;
+            }
         }
-        if(i < 0)
-        {
-            return 0;
-        }
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int take = 0;
-        int skip = sol(i+1,j,coins,dp);
-        if(coins[i] <= j)
-        {
-            take = sol(i,j-coins[i],coins,dp);
-        }
-        return dp[i][j] = take + skip;
+        return dp[n][amount];
     }
 }
