692a455bfb67bb1ba12dbebe3e102f193705b0b1
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-05-14 22:17:51 +0530

    Time: 1 ms (100%), Space: 46.7 MB (24.61%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -1,27 +1,64 @@
 class Solution {
-    public int[] separateDigits(int[] nums) {
-        int n = nums.length;
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int num : nums)
+    public int[] separateDigits(int[] arr) {
+        int totalDigits = 0;
+        for (int n : arr) 
         {
-            ArrayList<Integer> temp = new ArrayList<>();
-            while(num>0)
+            int temp = n;
+
+            if (temp == 0) totalDigits++;
+            else 
             {
-                int dig = num%10;
-                temp.add(dig);
-                num = num/10;
-            }
-            for(int i=0; i<temp.size(); i++)
-            {
-                list.add(temp.get(temp.size()-i-1));
+                while (temp > 0) 
+                {
+                    totalDigits++;
+                    temp /= 10;
+                }
             }
         }
 
-        int[] ans = new int[list.size()];
-        for(int i=0; i<list.size(); i++)
+        int[] ans = new int[totalDigits];
+        int index = totalDigits - 1;
+        for (int i = arr.length - 1; i >= 0; i--) 
         {
-            ans[i] = list.get(i);
+            int num = arr[i];
+            if(num == 0) ans[index--] = 0;
+            else
+            {
+                while (num > 0) 
+                {
+                    ans[index--] = num % 10;
+                    num /= 10;
+                }
+            }
         }
         return ans;
     }
 }
+// class Solution {
+//     public int[] separateDigits(int[] nums) {
+//         int n = nums.length;
+//         ArrayList<Integer> list = new ArrayList<>();
+//         for(int num : nums)
+//         {
+//             ArrayList<Integer> temp = new ArrayList<>();
+//             while(num>0)
+//             {
+//                 int dig = num%10;
+//                 temp.add(dig);
+//                 num = num/10;
+//             }
+//             for(int i=0; i<temp.size(); i++)
+//             {
+//                 list.add(temp.get(temp.size()-i-1));
+//             }
+//         }
+
+//         int[] ans = new int[list.size()];
+//         for(int i=0; i<list.size(); i++)
+//         {
+//             ans[i] = list.get(i);
+//         }
+//         return ans;
+//     }
+// }
+
