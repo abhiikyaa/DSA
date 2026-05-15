4dee2c577eda810141a76b306404a62752a7235b
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-05-15 11:18:45 +0530

    Time: 2 ms (53.76%), Space: 46.5 MB (27.58%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,24 +1,29 @@
 class Solution {
-    public void dfs (int node,List<List<Integer>> rooms, boolean[] visited){
-        visited[node] = true;
-        for(int nbr : rooms.get(node)){
-            if(!visited[nbr]){
-                dfs(nbr, rooms, visited);
+    public boolean bfs (List<List<Integer>> rooms, boolean[] visited){
+        Queue < Integer> q = new LinkedList<>();
+        q.offer(0);
+        visited[0] = true;
 
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(int nbr : rooms.get(node)){
+                if(visited[nbr] == false){
+                    visited[nbr] = true;
+                    q.add(nbr);
+                }
             }
         }
+        for(boolean unlocked : visited){
+            if(!unlocked){
+                return false;
+            }
+        }    
+        return true;
+
     }
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean[] visited = new boolean[rooms.size()];
-        int n = rooms.size();
-        dfs(0,rooms,visited);
-
-        for(int i = 0;i < n;i++){
-            if(visited[i] == false){
-                return false;
-            }
-        }
-        return true;
+        return bfs(rooms, visited);
         
     }
 }
