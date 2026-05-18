a570f718fa85a0ad4d81bc7b175ab822d1dffd81
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-05-18 12:30:28 +0530

    Time: 2 ms (10.81%), Space: 46.3 MB (96.87%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,25 +1,53 @@
 class Solution {
+    class Pair
+    {
+        int row;
+        int col;
+        Pair(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+    }
     int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
-
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int originalcolor = image[sr][sc];
-        if(originalcolor == color) return image;
-
-        dfs(image, sr, sc, originalcolor, color);
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) 
+    {
+        int n = image.length;
+        int m = image[0].length;
+        boolean[][] vis = new boolean[n][m];
+        int c = image[sr][sc];
+        if(c == color)
+        {
+            return image;
+        }
+        bfs(image,sr,sc,c,color,vis);
         return image;
     }
 
-    public void dfs(int[][] image, int r, int c, int originalcolor, int newcolor){
-        if(r < 0 || c < 0 || r >= image.length || c >= image[0].length  || image[r][c] != originalcolor){
-            return;
-        }
-
-        image[r][c] = newcolor;
-
-        for(int[] d : dir){
-            int nr = r + d[0];
-            int nc = c + d[1];
-            dfs(image, nr, nc, originalcolor, newcolor);
+    public void bfs(int[][] image, int sr, int sc,int c, int newc, boolean[][] vis)
+    {
+        Queue <Pair> q = new LinkedList<>();
+        q.add(new Pair(sr,sc));
+        vis[sr][sc] = true;
+        image[sr][sc] = newc;
+        
+        while(!q.isEmpty())
+        {
+            Pair curr = q.poll();
+            for(int[] d : dir )
+            {
+                int nr = curr.row + d[0];
+                int nc = curr.col + d[1];
+                
+                if(nr >= 0 && nc >= 0 && nr < image.length && nc < image[0].length && !vis[nr][nc] && image[nr][nc] == c)
+                {
+                    vis[nr][nc] = true;
+                    image[nr][nc] = newc;
+                    q.add(new Pair(nr,nc));
+                    
+                }
+            }
         }
     }
+    
 }
