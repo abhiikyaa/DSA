3eefb1087241cf940a058c2bf7fa12389813c9b7
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-06-07 14:16:17 +0530

    Time: 0 ms (100%), Space: 42.7 MB (72.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return MaxAmt(nums, nums.length-1, dp);
+        int n = nums.length;
+        if(n == 0) return 0;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return solve(nums,0,dp);
     }
-    public int MaxAmt(int[] nums, int n, int[]dp){
-        if(n<0) return 0;
-
-        if(n == 0) return nums[0];
-
-        if(n == 1) return Math.max(nums[0],nums[1]);
-
-        if(dp[n] != -1) return dp[n];
-
-        dp[n] = Math.max(nums[n] + MaxAmt(nums, n-2, dp) , MaxAmt(nums, n - 1 , dp));
-        return dp[n];
+    public int solve(int[] nums, int i, int[] dp)
+    {
+        if(i >= nums.length) return 0;
+        if(dp[i] != -1) return dp[i];
+        int take = nums[i] + solve(nums,i+2,dp);
+        int skip = solve(nums,i+1,dp);
+        return dp[i] = Math.max(take,skip);
+        
+        
     }
 }
