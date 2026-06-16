0cbf20eea20df95242ad4880dfb938eccc9729d4
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-06-16 23:09:05 +0530

    Time: 0 ms (100%), Space: 43 MB (5.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,11 +1,13 @@
 class Solution {
-    public int rob(int[] nums) {
+    public int rob(int[] nums) 
+    {
         int n = nums.length;
         if(n == 0) return 0;
         int[] dp = new int[n];
         Arrays.fill(dp,-1);
         return solve(nums,0,dp);
     }
+
     public int solve(int[] nums, int i, int[] dp)
     {
         if(i >= nums.length) return 0;
@@ -13,7 +15,5 @@ class Solution {
         int take = nums[i] + solve(nums,i+2,dp);
         int skip = solve(nums,i+1,dp);
         return dp[i] = Math.max(take,skip);
-        
-        
     }
 }
