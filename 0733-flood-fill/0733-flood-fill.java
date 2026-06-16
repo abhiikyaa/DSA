9155f15e969c48794b251c725ef07793ffdbfaff
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-06-16 14:49:10 +0530

    Time: 2 ms (11.49%), Space: 47 MB (25.11%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -19,35 +19,34 @@ class Solution {
         if(c == color)
         {
             return image;
-        }
-        bfs(image,sr,sc,c,color,vis);
+        }   
+        bfs(image,sr,sc,color,c,vis);
         return image;
     }
 
-    public void bfs(int[][] image, int sr, int sc,int c, int newc, boolean[][] vis)
+    public void bfs(int[][] image, int sr, int sc, int color, int c, boolean[][] vis)
     {
-        Queue <Pair> q = new LinkedList<>();
+        Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(sr,sc));
         vis[sr][sc] = true;
-        image[sr][sc] = newc;
-        
+        image[sr][sc] = color;
+
         while(!q.isEmpty())
         {
             Pair curr = q.poll();
-            for(int[] d : dir )
+            for(int[] d : dir)
             {
                 int nr = curr.row + d[0];
                 int nc = curr.col + d[1];
-                
-                if(nr >= 0 && nc >= 0 && nr < image.length && nc < image[0].length && !vis[nr][nc] && image[nr][nc] == c)
-                {
-                    vis[nr][nc] = true;
-                    image[nr][nc] = newc;
-                    q.add(new Pair(nr,nc));
-                    
-                }
+            
+
+            if(nr >= 0 && nc >= 0 && nr < image.length && nc < image[0].length && !vis[nr][nc] && image[nr][nc] == c)
+            {
+                vis[nr][nc] = true;
+                image[nr][nc] = color;
+                q.add(new Pair(nr,nc));
+            }
             }
         }
     }
-    
 }
