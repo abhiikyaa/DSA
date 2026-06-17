f6e56e8d1184a48a9888d1331b60cbeef7ddc159
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-06-17 14:26:21 +0530

    Time: 125 ms (6.45%), Space: 47.6 MB (8.27%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,22 +1,20 @@
 class Solution {
-    static int INF = (int)1e9;
-    public int jump(int[] nums) 
-    {
+    public int jump(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
+        int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-
-        return solve(nums,0,dp);
+        return solve(0,nums,dp);
     }
 
-    public int solve(int[] nums, int i, int[] dp)
+    public int solve(int i, int[] nums, int[] dp)
     {
         if(i >= nums.length-1) return 0;
         if(dp[i] != -1) return dp[i];
-        int min = INF;
+        int min = (int)1e9;
+        int take = 0;
         for(int j = 1; j <= nums[i] && i+j < nums.length; j++)
         {
-            int take = 1 + solve(nums,i+j,dp);
+            take = 1 + solve(i+j,nums,dp);
             min = Math.min(min,take);
         }
         return dp[i] = min;
