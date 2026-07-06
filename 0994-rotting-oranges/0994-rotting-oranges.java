ff6d75964dc579513abee042f580e654f4809554
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-06 15:25:47 +0530

    Time: 3 ms (10.68%), Space: 44.3 MB (24.76%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,53 +1,65 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
+    class Pair
+    {
+        int row;
+        int col;
+        int time;
+        Pair(int row, int col, int time)
+        {
+            this.row = row;
+            this.col = col;
+            this.time = time;
+        }
+    }
+    static int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+    public int orangesRotting(int[][] grid) 
+    {
         int n = grid.length;
         int m = grid[0].length;
-        int freshorange = 0;
-        int time = 0;
-
-        Queue<int[]> q = new LinkedList<>();
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i] [j] == 2){
-                    q.add(new int[]{i, j , 0});
+        int fo = 0;
+        Queue<Pair> q = new LinkedList<>();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == 2){
+                    q.add(new Pair(i,j,0));
                 }
-                if(grid[i] [j] == 1){
-                    freshorange++;
-                }
-
-            }
-        }
-        if(freshorange == 0) return 0;
-        int[][] dr = {{0,-1}, {-1,0}, {0,1}, {1,0}};
-
-        while(!q.isEmpty()){
-            int qsize = q.size();
-            boolean rotted = false;
-            for (int k = 0; k < qsize; k++){
-                int[] curr = q.poll();
-                int r = curr[0];
-                int c = curr[1];
-
-                for(int i = 0; i < 4; i++){
-                    int nr = r + dr[i][0];
-                    int nc = c + dr[i][1];
-
-                    if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1){
-                        grid[nr][nc] = 2;
-                        freshorange--;
-                        q.add(new int[]{nr,nc});
-                        rotted = true;
-                    }
+                else if(grid[i][j] == 1)
+                {
+                    fo++;
                 }
             }
-            if (rotted) time++;
         }
-        if (freshorange == 0) {
-            return time;
-        }else{
-            return -1;
-        }
+        int ans = bfs(grid,q,fo,n,m);
+        return ans;
 
+    }
+
+    private int bfs(int[][] grid, Queue<Pair> q, int fo, int n, int m)
+    {
+        int t = 0;
+        while(!q.isEmpty())
+        {
+            Pair curr = q.poll();
+            int r = curr.row;
+            int c = curr.col;
+            t = curr.time;
+
+            for(int[] d : dir)
+            {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == 1)
+                {
+                    grid[nr][nc] = 2;
+                    fo--;
+                    q.add(new Pair(nr,nc,t+1));
+                }
+            }
+        }
+        if(fo == 0) return t;
+        else return -1;
     }
 }
