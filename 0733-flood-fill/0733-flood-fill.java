e8980898b703000e046f188de3f6fa3829bd06f2
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-06 16:09:55 +0530

    Time: 2 ms (11.99%), Space: 46.9 MB (43.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -9,26 +9,21 @@ class Solution {
             this.col = col;
         }
     }
-    int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+    static int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) 
     {
         int n = image.length;
         int m = image[0].length;
-        boolean[][] vis = new boolean[n][m];
         int c = image[sr][sc];
-        if(c == color)
-        {
-            return image;
-        }   
-        bfs(image,sr,sc,color,c,vis);
+        if(c == color) return image;
+        bfs(image,sr,sc,color,c,n,m);
         return image;
     }
 
-    public void bfs(int[][] image, int sr, int sc, int color, int c, boolean[][] vis)
+    public void bfs(int[][] image, int sr, int sc, int color, int c,int n, int m)
     {
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(sr,sc));
-        vis[sr][sc] = true;
         image[sr][sc] = color;
 
         while(!q.isEmpty())
@@ -38,14 +33,11 @@ class Solution {
             {
                 int nr = curr.row + d[0];
                 int nc = curr.col + d[1];
-            
-
-            if(nr >= 0 && nc >= 0 && nr < image.length && nc < image[0].length && !vis[nr][nc] && image[nr][nc] == c)
-            {
-                vis[nr][nc] = true;
-                image[nr][nc] = color;
-                q.add(new Pair(nr,nc));
-            }
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m && image[nr][nc] == c)
+                {
+                    image[nr][nc] = color;
+                    q.add(new Pair(nr,nc));
+                }
             }
         }
     }
