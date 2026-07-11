c59245247a8be085635448e0b9a1909895df7a5d
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-11 22:30:17 +0530

    Time: 2 ms (91.19%), Space: 44 MB (84.81%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -11,51 +11,48 @@ class Solution {
             this.time = time;
         }
     }
-    static int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+    int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
     public int orangesRotting(int[][] grid) 
     {
         int n = grid.length;
         int m = grid[0].length;
         int fo = 0;
-        Queue<Pair> q = new LinkedList<>();
+
+        Queue <Pair> q = new LinkedList<>();
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
-                if(grid[i][j] == 2){
+                if(grid[i][j] == 2)
+                {
                     q.add(new Pair(i,j,0));
                 }
-                else if(grid[i][j] == 1)
+                else if(grid[i][j] == 1 )
                 {
                     fo++;
                 }
             }
         }
-        int ans = bfs(grid,q,fo,n,m);
+        int ans = bfs(grid,q,n,m,fo);
         return ans;
-
+        
     }
-
-    private int bfs(int[][] grid, Queue<Pair> q, int fo, int n, int m)
+    public int bfs(int[][] grid, Queue<Pair> q, int n, int m,int fo)
     {
         int t = 0;
         while(!q.isEmpty())
         {
             Pair curr = q.poll();
-            int r = curr.row;
-            int c = curr.col;
             t = curr.time;
-
             for(int[] d : dir)
             {
-                int nr = r + d[0];
-                int nc = c + d[1];
-
+                int nr = curr.row + d[0];
+                int nc = curr.col + d[1];
                 if(nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == 1)
                 {
                     grid[nr][nc] = 2;
-                    fo--;
                     q.add(new Pair(nr,nc,t+1));
+                    fo--;
                 }
             }
         }
