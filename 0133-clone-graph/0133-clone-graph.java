e4252dffc68d2598ad96e0516b9caa38fa721158
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-11 18:59:26 +0530

    Time: 30 ms (7.15%), Space: 44.1 MB (76.8%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,37 +1,39 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
-    HashMap<Node, Node> map = new HashMap<>();
+    class Pair {
+        Node original;
+        Node clone;
+
+        Pair(Node original, Node clone) {
+            this.original = original;
+            this.clone = clone;
+        }
+    }
+
     public Node cloneGraph(Node node) {
         if (node == null) return null;
-        if (map.containsKey(node)) 
-        {
-            return map.get(node);
-        }
+
+        HashMap<Node, Node> m = new HashMap<>();
+        Queue<Pair> q = new LinkedList<>();
+
         Node clone = new Node(node.val);
-        map.put(node, clone);
-        for (Node nei : node.neighbors) 
-        {
-            clone.neighbors.add(cloneGraph(nei));
+        m.put(node, clone);
+        q.offer(new Pair(node, clone));
+
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+
+            for (Node nbr : curr.original.neighbors) {
+
+                if (!m.containsKey(nbr)) {
+                    Node newClone = new Node(nbr.val);
+                    m.put(nbr, newClone);
+                    q.offer(new Pair(nbr, newClone));
+                }
+
+                curr.clone.neighbors.add(m.get(nbr));
+            }
         }
+
         return clone;
     }
 }
