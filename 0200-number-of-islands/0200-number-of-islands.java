28ca9a0f60cc2e310117d7e54978661b3c66d80c
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-11 22:00:35 +0530

    Time: 5 ms (29.22%), Space: 51.2 MB (92.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -9,44 +9,41 @@ class Solution {
             this.col = col;
         }
     }
-    static int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
+    int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
     public int numIslands(char[][] grid) 
     {
         int n = grid.length;
         int m = grid[0].length;
         boolean[][] vis = new boolean[n][m];
-        int c = 0;
+        int count = 0;
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
-                if(grid[i][j] == '1' && !vis[i][j])
+                if(grid[i][j] == '1' && vis[i][j] == false)
                 {
-                    c++;
-                    bfs(grid,vis,i,j,n,m);
+                    bfs(grid,i,j,vis);
+                    count++;
                 }
             }
         }
-        return c;
+        return count;
     }
 
-    public void bfs(char[][] grid,boolean[][] vis,int i, int j, int n, int m)
+    public void bfs(char[][] grid, int r, int c, boolean[][] vis)
     {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
-        vis[i][j] = true;
-
+        Queue <Pair> q = new LinkedList<>();
+        q.add(new Pair(r,c));
+        vis[r][c] = true;
+        grid[r][c] = '0';
         while(!q.isEmpty())
         {
             Pair curr = q.poll();
-            int r = curr.row;
-            int c = curr.col;
-
             for(int[] d : dir)
             {
-                int nr = r + d[0];
-                int nc = c + d[1];
-                if(nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == '1' && !vis[nr][nc])
+                int nr = curr.row + d[0];
+                int nc = curr.col + d[1];
+                if(nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == '1' && vis[nr][nc] == false)
                 {
                     grid[nr][nc] = '0';
                     vis[nr][nc] = true;
