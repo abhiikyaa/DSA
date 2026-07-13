289f21f86aa646794e22e2c0b5ec2636b3d01ea4
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-13 23:32:03 +0530

    Time: 8 ms (69.87%), Space: 52.6 MB (96.67%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,58 +1,74 @@
 class Solution {
-    class Pair{
+    class Pair
+    {
         int row;
         int col;
-        Pair(int row, int col){
-            this.row= row;
+        Pair(int row, int col)
+        {
+            this.row = row;
             this.col = col;
         }
     }
-    int dir[][] = {{0,1},{1,0},{-1,0},{0,-1}};
-    public int numEnclaves(int[][] grid) {
+    int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+    public int numEnclaves(int[][] grid) 
+    {
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] visited = new boolean[n][m];
-        for(int i = 0; i < n; i++){
-            if(grid[i][0] == 1 && visited[i][0] == false){
-                bfs(grid, visited, i, 0);
+        boolean[][] vis = new boolean[n][m];
+
+        for(int i = 0; i < n; i++)
+        {
+            if(grid[i][0] == 1 && !vis[i][0])
+            {
+                bfs(grid,i,0,vis);
             }
-            if(grid[i][m-1] == 1 && visited[i][m-1] == false){
-                bfs(grid, visited, i , m-1);
+            if(grid[i][m-1] == 1 && !vis[i][m-1])
+            {
+                bfs(grid,i,m-1,vis);
             }
         }
 
-        for(int j =0; j < m; j++){
-            if(grid[0][j] == 1 && visited[0][j] == false){
-                bfs(grid, visited, 0, j);
+        for(int j = 0; j < m; j++)
+        {
+            if(grid[0][j] == 1 && !vis[0][j])
+            {
+                bfs(grid,0,j,vis);
             }
-            if(grid[n-1][j] == 1 && visited[n-1][j] == false){
-                bfs(grid, visited, n-1, j);
+            if(grid[n-1][j] == 1 && !vis[n-1][j])
+            {
+                bfs(grid,n-1,j,vis);
             }
         }
 
-        int count = 0;
-        for(int i =0; i < n-1;i++){
-            for(int j = 0; j < m-1; j++){
-                if(grid[i][j] == 1 && visited[i][j] == false){
-                    count++;
+        int cnt = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == 1 && !vis[i][j])
+                {
+                    cnt++;
                 }
             }
         }
-        return count;
+        return cnt;
     }
-    public void bfs(int[][] grid,boolean[][] visited, int r, int c ){
+
+    public void bfs(int[][] grid, int r, int c, boolean[][] vis)
+    {
         Queue <Pair> q = new LinkedList<>();
         q.add(new Pair(r,c));
-        visited[r][c] = true;
-
-        while(!q.isEmpty()){
+        vis[r][c] = true;
+        while(!q.isEmpty())
+        {
             Pair curr = q.poll();
-            for(int[] d : dir){
+            for(int[] d : dir)
+            {
                 int nr = curr.row + d[0];
                 int nc = curr.col + d[1];
-
-                if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == 1 && visited[nr][nc] == false){
-                    visited[nr][nc] = true;
+                if(nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 1 && !vis[nr][nc])
+                {
+                    vis[nr][nc] = true;
                     q.add(new Pair(nr,nc));
                 }
             }
