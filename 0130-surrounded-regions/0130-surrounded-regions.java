368dee6f7ee7efaa83f3b6822c7dc50691be92f1
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-13 23:16:09 +0530

    Time: 3 ms (27.37%), Space: 48.1 MB (10.87%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,61 +1,78 @@
 class Solution {
-    class Pair {
+    class Pair
+    {
         int row;
         int col;
-        Pair(int row, int col) {
+        Pair(int row, int col)
+        {
             this.row = row;
             this.col = col;
         }
     }
-
-    int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
-
-    public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
-        boolean[][] visited = new boolean[n][m];
-        for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O' && !visited[0][j]) {
-                bfs(board, 0, j, visited);
+    int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+    public void solve(char[][] grid) 
+    {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] vis = new boolean[n][m]; 
+        for(int i = 0; i < n; i++)
+        {
+            if(grid[i][0] == 'O' && !vis[i][0])
+            {
+                bfs(grid,i,0,vis);
             }
-            if (board[n-1][j] == 'O' && !visited[n-1][j]) {
-                bfs(board, n-1, j, visited);
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O' && !visited[i][0]) {
-                bfs(board, i, 0, visited);
-            }
-            if (board[i][m-1] == 'O' && !visited[i][m-1]) {
-                bfs(board, i, m-1, visited);
+            if(grid[i][m-1] == 'O' && !vis[i][m-1])
+            {
+                bfs(grid,i,m-1,vis);
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'O' && !visited[i][j]) {
-                    board[i][j] = 'X';
+
+        for(int j = 0; j < m; j++)
+        {
+            if(grid[0][j] == 'O' && !vis[0][j])
+            {
+                bfs(grid,0,j,vis);
+            }
+            if(grid[n-1][j] == 'O' && !vis[n-1][j])
+            {
+                bfs(grid,n-1,j,vis);
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == 'O' && !vis[i][j])
+                {
+                    grid[i][j] = 'X';
                 }
             }
         }
+        
     }
 
-    public void bfs(char[][] board, int r, int c, boolean[][] visited) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(r, c));
-        visited[r][c] = true;
-
-        while (!q.isEmpty()) {
+    public void bfs(char[][] grid, int r, int c, boolean[][] vis)
+    {
+        Queue <Pair> q = new LinkedList<>();
+        q.add(new Pair(r,c));
+        vis[r][c] = true;
+        while(!q.isEmpty())
+        {
             Pair curr = q.poll();
-
-            for (int[] d : dir) {
+            for(int[] d : dir)
+            {
                 int nr = curr.row + d[0];
                 int nc = curr.col + d[1];
-
-                if (nr >= 0 && nr < board.length && nc >= 0 && nc < board[0].length && board[nr][nc] == 'O' && !visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    q.add(new Pair(nr, nc));
+                if(nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 'O' && !vis[nr][nc])
+                {
+                    vis[nr][nc] = true;
+                    q.add(new Pair(nr,nc));
                 }
             }
         }
+
     }
 }
+
+    
