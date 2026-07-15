7b4c4897677b214021e9d31f62eb488002157f6e
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-15 12:24:15 +0530

    Time: 25 ms (43.11%), Space: 60.5 MB (85.2%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,40 +1,51 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n =graph.length;
+        int n = graph.length;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++)
+        {
             adj.add(new ArrayList<>());
         }
 
         int[] inDeg = new int[n];
-        for(int i = 0; i < n; i++){
-            for(int edge : graph[i]){
-                adj.get(edge).add(i);
-                inDeg[i]++;
-            }
+        for(int i = 0; i < n; i++)
+        {
+        for(int g : graph[i])
+        {
+            adj.get(g).add(i);
+            inDeg[i]++;
+        }
         }
 
-        Queue<Integer> q =new LinkedList<>();
-        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList <Integer> ans = new ArrayList<>();
+        bfs(adj,n,inDeg,ans);
+        Collections.sort(ans);
+        return ans;
+    }
 
-        for(int i =0; i < n; i++){
-            if(inDeg[i] == 0){
+    public void bfs(ArrayList<ArrayList<Integer>> adj, int n, int[] inDeg, ArrayList <Integer> ans)
+    {
+        Queue <Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++)
+        {
+            if(inDeg[i] == 0)
+            {
                 q.add(i);
             }
         }
 
-        while(!q.isEmpty()){
-            int node = q.poll();
-            ans.add(node);
-
-            for(int nbr : adj.get(node)){
+        while(!q.isEmpty())
+        {
+            int curr = q.poll();
+            for(int nbr : adj.get(curr))
+            {
                 inDeg[nbr]--;
-                if(inDeg[nbr] == 0){
+                if(inDeg[nbr] == 0)
+                {
                     q.add(nbr);
                 }
             }
+            ans.add(curr);
         }
-        Collections.sort(ans);
-        return ans;
     }
 }
