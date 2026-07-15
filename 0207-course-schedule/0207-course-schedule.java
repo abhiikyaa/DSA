8ffc1c8c50739c7117546b2fb07a9ecf89afb49b
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-15 17:26:26 +0530

    Time: 7 ms (56.76%), Space: 47 MB (78.57%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,35 +1,41 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> g = new ArrayList<>();
-        for(int i = 0; i < numCourses; i++){
-            g.add(new ArrayList<>());
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++)
+        {
+            adj.add(new ArrayList<>());
         }
-
-        int[] indeg = new int[numCourses];
-        for(int[] p : prerequisites){
-            g.get(p[1]).add(p[0]);
-            indeg[p[0]]++;
+        
+        int[] inDeg = new int[numCourses];
+        for(int[] e : prerequisites)
+        {
+            int u = e[0];
+            int v = e[1];
+            adj.get(u).add(v);
+            inDeg[v]++;
         }
+        
+        return bfs(adj,numCourses,inDeg);
+    }
+    public boolean bfs(ArrayList<ArrayList<Integer>> adj,int numCourses, int[] inDeg)
+    {
         Queue <Integer> q = new LinkedList<>();
-        for(int i =0; i < numCourses; i++){
-            if(indeg[i] == 0){
-                q.add(i);
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(inDeg[i] == 0) q.add(i);
+        }
+        int cnt = 0;
+        while(!q.isEmpty())
+        {
+            int curr = q.poll();
+            cnt++;
+            for(int nbr : adj.get(curr))
+            {
+                inDeg[nbr]--;
+                if(inDeg[nbr] == 0) q.add(nbr);
             }
         }
-
-        int count = 0;
-        while(!q.isEmpty()){
-            int node = q.poll();
-            count++;
-
-            for(int nbr : g.get(node)){
-                indeg[nbr]--;
-                if(indeg[nbr] == 0){
-                    q.add(nbr);
-                }
-            }
-
-        }
-        return count == numCourses;
+        if(cnt == numCourses) return true;
+        else return false;
     }
 }
