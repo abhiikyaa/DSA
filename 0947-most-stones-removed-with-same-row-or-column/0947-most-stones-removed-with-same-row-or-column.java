3d2bf97e2a39593f715892f4ea79698557d9bd44
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-07-19 11:49:23 +0530

    Time: 31 ms (45.37%), Space: 46.5 MB (88.66%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,54 +1,61 @@
 class Solution {
-    class DSU{
+    class DSU {
         int[] parent;
         int[] rank;
-        DSU(int n){
+
+        DSU(int n) {
             parent = new int[n];
             rank = new int[n];
-            for(int i = 0; i < n; i++){
+
+            for (int i = 0; i < n; i++) {
                 parent[i] = i;
             }
         }
-        int find(int x){
-            if(parent[x]!=x){
+
+        int find(int x) {
+            if (parent[x] != x) {
                 parent[x] = find(parent[x]);
             }
             return parent[x];
         }
-        void union(int a, int b){
+
+        void union(int a, int b) {
             int pa = find(a);
             int pb = find(b);
-            if(pa == pb) return;
-            if(rank[pa] > rank[pb]){
-                parent[pb] = pa;
-            }
-            else if(rank[pa] < rank[pb]){
+            if (pa == pb)
+                return;
+            if (rank[pa] < rank[pb])
                 parent[pa] = pb;
+            else if (rank[pb] < rank[pa])
+                parent[pb] = pa;
+            else {
+                parent[pb] = pa;
+                rank[pa]++;
             }
-            else{
-                if(rank[pa] == rank[pb]){
-                    parent[pb] = pa;
-                    rank[pa]++;
-                }
-            }
-
         }
     }
+
     public int removeStones(int[][] stones) {
         int n = stones.length;
-        DSU ans = new DSU(n);
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
-                    ans.union(i, j);
+        DSU dsu = new DSU(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    dsu.union(i, j);
                 }
             }
         }
 
-        Set<Integer> s = new HashSet<>();
-        for(int i = 0; i < n; i++){
-            s.add(ans.find(i));
+        int c = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(dsu.find(i) == i)
+            {
+                c++;
+            }
         }
-        return n - s.size();
+
+        return n - c;
+
     }
 }
