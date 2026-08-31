e5b25c950edcb695f3083e24ae71081f8675f662
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-09-01 02:33:58 +0530

    Time: 8 ms (99.64%), Space: 62 MB (25.11%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,37 +1,113 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode slow = head, fast = head.next;
-        while (fast != null && fast.next != null) {
+    public ListNode sortList(ListNode head) 
+    {
+        if(head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null)
+        {
             slow = slow.next;
             fast = fast.next.next;
         }
-        
-        ListNode mid = slow.next;
+
+        ListNode mid = slow;
+
+        ListNode list1 = head;
+        ListNode list2 = slow.next;
         slow.next = null;
-        
-        ListNode left = sortList(head);
-        ListNode right = sortList(mid);
-        
-        return merge(left, right);
+        // ListNode tail1 = null;
+        // ListNode tail2 = null;
+        // ListNode curr = head;
+        // ListNode next;
+
+        // while(curr != null)
+        // {
+        //     next = curr.next;
+        //     curr.next = null;
+        //     if(curr.val <= mid.val)
+        //     {
+        //         if(list1 == null)
+        //         {
+        //             list1 = curr;
+        //             tail1 = curr;
+        //         }
+        //         else
+        //         {
+        //             tail1.next = curr;
+        //             tail1 = curr;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         if(list2 == null)
+        //         {
+        //             list2 = curr;
+        //             tail2 = curr;
+        //         }
+        //         else
+        //         {
+        //             tail2.next = curr;
+        //             tail2 = curr;
+        //         }
+        //     }
+        //     curr = next;
+        // }
+        list1 = sortList(list1);
+        list2 = sortList(list2);
+        return merge(list1,list2);
     }
-    
-    private ListNode merge(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                tail.next = l1;
-                l1 = l1.next;
-            } else {
-                tail.next = l2;
-                l2 = l2.next;
-            }
-            tail = tail.next;
+    public ListNode merge(ListNode list1, ListNode list2) 
+    {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        ListNode head;
+
+        if(list1.val <= list2.val)
+        {
+            head = list1;
+            list1 = list1.next;
         }
-        
-        tail.next = (l1 != null) ? l1 : l2;
-        return dummy.next;
+        else
+        {
+            head = list2;
+            list2 = list2.next; 
+        }
+
+        ListNode curr = head;
+        while(list1 != null && list2 != null)
+        {
+            if(list1.val <= list2.val)
+            {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        if(list1 != null)
+        {
+            curr.next = list1;
+        }
+        else
+        {
+            curr.next = list2;
+        }
+        return head;
     }
 }
