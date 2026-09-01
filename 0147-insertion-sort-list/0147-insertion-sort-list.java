546b9860259eb079ba24d29227753a1545282598
Ravi Ranjan Kumar R <abhiikumar3024@gmail.com>
2026-09-01 23:32:46 +0530

    Time: 17 ms (82.17%), Space: 46.1 MB (94.45%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -8,106 +8,90 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode insertionSortList(ListNode head) 
+//     {
+//         if(head == null || head.next == null) return head;
+//         ListNode curr = head.next;
+//         ListNode next;
+
+//         while(curr != null)
+//         {
+//             next = curr.next;
+//             curr.next = null;
+//             ListNode prev = null;
+//             ListNode temp = head;
+//             while(temp != curr && temp.val <= curr.val)
+//             {
+//                 prev = temp;
+//                 temp = temp.next;
+//             }
+//             if(prev == null)
+//             {
+//                 curr.next = head;
+//                 head = curr;
+//             }
+//             else if(temp != curr)
+//             {
+//                 prev.next = curr;
+//                 curr.next = temp;
+//             }
+//             curr = next;
+//         }  
+//         return head; 
+//     }
+// }
+
 class Solution {
     public ListNode insertionSortList(ListNode head) 
     {
         if(head == null || head.next == null) return head;
-        ListNode slow = head;
-        ListNode fast = head.next;
 
-        while(fast != null && fast.next != null)
+        ListNode curr = head.next;
+        ListNode prevCurr = head;
+
+        while(curr != null)
         {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
+            ListNode next = curr.next;
 
-        ListNode mid = slow;
+            ListNode prev = null;
+            ListNode temp = head;
 
-        ListNode list1 = head;
-        ListNode list2 = slow.next;
-        slow.next = null;
-        // ListNode tail1 = null;
-        // ListNode tail2 = null;
-        // ListNode curr = head;
-        // ListNode next;
-
-        // while(curr != null)
-        // {
-        //     next = curr.next;
-        //     curr.next = null;
-        //     if(curr.val <= mid.val)
-        //     {
-        //         if(list1 == null)
-        //         {
-        //             list1 = curr;
-        //             tail1 = curr;
-        //         }
-        //         else
-        //         {
-        //             tail1.next = curr;
-        //             tail1 = curr;
-        //         }
-        //     }
-        //     else
-        //     {
-        //         if(list2 == null)
-        //         {
-        //             list2 = curr;
-        //             tail2 = curr;
-        //         }
-        //         else
-        //         {
-        //             tail2.next = curr;
-        //             tail2 = curr;
-        //         }
-        //     }
-        //     curr = next;
-        // }
-        list1 = insertionSortList(list1);
-        list2 = insertionSortList(list2);
-        return merge(list1,list2);
-    }
-    public ListNode merge(ListNode list1, ListNode list2) 
-    {
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
-
-        ListNode head;
-
-        if(list1.val <= list2.val)
-        {
-            head = list1;
-            list1 = list1.next;
-        }
-        else
-        {
-            head = list2;
-            list2 = list2.next; 
-        }
-
-        ListNode curr = head;
-        while(list1 != null && list2 != null)
-        {
-            if(list1.val <= list2.val)
+            // insertion position find karo
+            while(temp != curr && temp.val <= curr.val)
             {
-                curr.next = list1;
-                list1 = list1.next;
+                prev = temp;
+                temp = temp.next;
             }
+
+            // curr already correct position par hai
+            if(temp == curr)
+            {
+                prevCurr = curr;
+                curr = next;
+                continue;
+            }
+
+            // curr ko old position se remove karo
+            prevCurr.next = next;
+
+            // curr ko head par insert karo
+            if(prev == null)
+            {
+                curr.next = head;
+                head = curr;
+            }
+            // curr ko beech mein insert karo
             else
             {
-                curr.next = list2;
-                list2 = list2.next;
+                prev.next = curr;
+                curr.next = temp;
             }
-            curr = curr.next;
+
+            curr = next;
         }
-        if(list1 != null)
-        {
-            curr.next = list1;
-        }
-        else
-        {
-            curr.next = list2;
-        }
+
         return head;
     }
 }
+
